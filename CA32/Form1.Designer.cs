a7b3c6438fcb9e459136e3027f4d1e3913ab333b
加藤雄大 <iamcat.6.poetry@gmail.com>
2019-10-06 18:39:05 +0900

    プロジェクト ファイルを追加します。

---
+++ CA32/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace CA32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(512, 512);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(512, 512);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 530);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reflesh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 530);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Run/Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 577);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Init(1)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(136, 577);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Init(50)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(260, 577);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 41);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Init(99)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 627);
+            this.trackBar1.Maximum = 65535;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(512, 90);
+            this.trackBar1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(328, 535);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 31);
+            this.textBox1.TabIndex = 7;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 717);
+            this.trackBar2.Maximum = 65535;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(512, 90);
+            this.trackBar2.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(541, 819);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "CA32";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CA32/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CA32
+{
+    public partial class Form1 : Form
+    {
+        Random _r = new Random();
+        byte[,,,,] _table = new byte[Param.STATE_SIZE, Param.STATE_SIZE, Param.STATE_SIZE, Param.STATE_SIZE, Param.STATE_SIZE];
+        byte[,] _caCur = new byte[Param.CA_SIZE, Param.CA_SIZE];
+        byte[,] _caNext = new byte[512, 512];
+        bool _update = true;
+        public Form1()
+        {
+            InitializeComponent();
+            refleshCA(50);
+            refleshTable();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (_update)
+            {
+                progressCA();
+                updateImage();
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            refleshTable();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            _update = !_update;
+        }
+        private void refleshCA(int prb)
+        {
+            for (int i = 0; i < Param.CA_SIZE; i++)
+            {
+                for (int j = 0; j < Param.CA_SIZE; j++)
+                {
+                    if(prb == 0)
+                    {
+                        _caCur[i, j] = 0;
+                    }
+                    else
+                    {
+                        _caCur[i, j] = (byte)(((prb - 1) < _r.Next(100)) ? 0 : (Param.STATE_SIZE - 1));
+                    }
+                }
+            }
+        }
+        private void refleshTable()
+        {
+            for(int a = 0; a < Param.STATE_SIZE; a++)
+            {
+                for (int b = 0; b < Param.STATE_SIZE; b++)
+                {
+                    for (int c = 0; c < Param.STATE_SIZE; c++)
+                    {
+                        for (int d = 0; d < Param.STATE_SIZE; d++)
+                        {
+                            for (int e = 0; e < Param.STATE_SIZE; e++)
+                            {
+                                _table[a, b, c, d, e] = (byte)((_r.Next(2) == 0) ? 0 : Param.STATE_SIZE - 1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void progressCA()
+        {
+            for(int i = 0;i < Param.CA_SIZE; i++)
+            {
+                for (int j = 0; j < Param.CA_SIZE; j++)
+                {
+                    byte a = (i == 0) ? _caCur[(Param.CA_SIZE - 1), j] : _caCur[i - 1, j];
+                    byte b = (j == 0) ? _caCur[i, (Param.CA_SIZE - 1)] : _caCur[i, j - 1];
+                    byte c = _caCur[i, j];
+                    byte d = (i == (Param.CA_SIZE - 1)) ? _caCur[0, j] : _caCur[i + 1, j];
+                    byte e = (j == (Param.CA_SIZE - 1)) ? _caCur[i, 0] : _caCur[i, j + 1];
+                    _caNext[i,j] = getNext(a, b, c, d, e);
+                }
+            }
+            for (int i = 0; i < Param.CA_SIZE; i++)
+            {
+                for (int j = 0; j < Param.CA_SIZE; j++)
+                {
+                    _caCur[i, j] = _caNext[i, j];
+                }
+            }
+        }
+        private byte getNext(int a, int b, int c, int d, int e)
+        {
+            return _table[a, b, c, d, e];
+        }
+        private void updateImage()
+        {
+            Bitmap bitmap = new Bitmap(Param.CA_SIZE, Param.CA_SIZE);
+
+            BitmapData data = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format32bppArgb);
+            byte[] buf = new byte[bitmap.Width * bitmap.Height * 4];
+            Marshal.Copy(data.Scan0, buf, 0, buf.Length);
+
+            for (int i = 0; i < Param.CA_SIZE; i++)
+            {
+                for (int j = 0; j < Param.CA_SIZE; j++)
+                {
+                    int lv = _caCur[i, j] * 16;
+                    buf[4 * (i * Param.CA_SIZE + j) + 0] = 0;//a
+//                    buf[4 * (i * Param.CA_SIZE + j) + 1] = (byte)lv;//r
+//                    buf[4 * (i * Param.CA_SIZE + j) + 2] = (byte)lv;//g
+                    buf[4 * (i * Param.CA_SIZE + j) + 3] = (byte)lv;//b
+                }
+            }
+            Marshal.Copy(buf, 0, data.Scan0, buf.Length);
+            bitmap.UnlockBits(data);
+
+
+/*
+            for (int i = 0; i < Param.CA_SIZE; i++)
+            {
+                for (int j = 0; j < Param.CA_SIZE; j++)
+                {
+                    int lv = _caCur[i, j] * 16;
+                    Color c = Color.FromArgb(lv, lv, lv);
+                    bitmap.SetPixel(i, j, c);
+                }
+            }
+*/
+            pictureBox1.Image = bitmap;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            refleshCA(5);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            refleshCA(50);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            refleshCA(95);
+        }
+    }
+    static class Param
+    {
+        public const int STATE_SIZE = 16;
+        public const int CA_SIZE = 512;
+    }
+}
